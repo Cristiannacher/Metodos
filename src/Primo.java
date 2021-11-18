05bdc0d1690e4428293978f6fdf5d4421135299c
Cristian <alunac6050@ieselcaminas.org>
2021-11-18 15:43:24 +0100

    Comit 25

---
+++ src/Primo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Primo {
+
+    public static boolean esPrimo(int num) {
+
+        boolean esprimo = true;
+
+        if (num % 2 == 0 && num != 2) {
+            esprimo = false;
+        } else {
+            for (int i = 3; i <num/2; i++) {
+                if ( num % i == 0) {
+                    esprimo = false;
+                }
+            }
+        }
+        return esprimo;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner introducir = new Scanner(System.in);
+        System.out.println("Introduce un numero para saber si es primo");
+        int num = introducir.nextInt();
+
+        if (esPrimo(num)) {
+            System.out.println("El numero " + num + " es primo");
+        } else System.out.println("El numero " + num + " no es primo");
+
+
+    }
+}
---
+++ src/Primos.java
@@ -0,0 +1,12 @@
+public class Primos {
+
+    public static void main(String[] args) {
+
+        for(int i =3; i<100; i++){
+            if(Primo.esPrimo(i)){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
